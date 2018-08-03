ecee59238ee9a8e9547ad8f7125f0e78d7c63d31
ShodowBot <42071279+ShodowBot@users.noreply.github.com>
2018-08-03 18:51:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,9 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -12534,4 +12537,9 @@ message.member.addRole(message.guild.roles.find("name", "100"));
   }
 	
 });
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
